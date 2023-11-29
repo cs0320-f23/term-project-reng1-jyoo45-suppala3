79cc6f45842b7fc5dfddd28aaa3488bd04fee3be
Jiwon <jiwon_yoo@brown.edu>
2023-11-29 16:16:26 -0500

    feat: running frontend server

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import "../styles/App.css";
+import REPL from "./Minesweeper";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="App-header">
+        <h1>Minesweeper</h1>
+      </p>
+      <REPL />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Minesweeper.tsx
@@ -0,0 +1,5 @@
+import "../styles/main.css";
+
+export default function Minesweeper() {
+  return <div className="game"></div>;
+}
